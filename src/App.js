39569632fb6909b83c0165935926fb1923b0ec8d
jinali99 <jinalisanghvi99@gmail.com>
2023-11-28 11:46:02 +0530

    start

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Footer from "./Components/Footer/footer";
+import Header from "./Components/Header/header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      React app
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Header/header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      THIS IS HEADER
+      <div>Logo goes here...</div>
+    </div>
+  );
+};
+
+export default Header;
