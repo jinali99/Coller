3ada95afe3f370a57b84892fce7d120db7390d6d
jinali99 <jinalisanghvi99@gmail.com>
2023-12-08 16:39:28 +0530

    page not found css fixed

--- src/Components/PageNotFound/pageNotFound.js
+++ src/Components/PageNotFound/pageNotFound.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import classes from './pageNotFound.module.css'
-import { TbError404 } from 'react-icons/tb'
 
 const PageNotFound = () => {
     return (
-        <div className={classes.notfound}>
-            <TbError404 color="#161a30" size={100} />
+        <div className={classes.notFound}>
+            <h2>404</h2>
             <p>Page Not Found</p>
         </div>
     )
