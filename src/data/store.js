3143b7a39b0805dee081376be26bb8eaef7e732d
jinali99 <jinalisanghvi99@gmail.com>
2023-12-11 12:16:35 +0530

    - persist cart

--- src/data/store.js
+++ src/data/store.js
@@ -1,16 +1,34 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import cartReducer from './reducer/cartSlice'
 import storage from 'redux-persist/lib/storage'
-import { persistReducer } from 'redux-persist'
+import {
+    FLUSH,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+    REHYDRATE,
+    persistReducer,
+    persistStore,
+} from 'redux-persist'
 
 const persistConfig = {
     key: 'root',
     storage,
 }
+const reducer = combineReducers({
+    cart: cartReducer,
+})
 
-const persistedReducer = persistReducer(persistConfig, cartReducer)
+const persistedReducer = persistReducer(persistConfig, reducer)
 
 export const store = configureStore({
     reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: false,
+        }),
 })
+export const persistor = persistStore(store)
+
 export default store
