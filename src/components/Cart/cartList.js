a635a89e159216586104ee8dee63e7d4b35a8cdf
jinali99 <jinalisanghvi99@gmail.com>
2023-12-11 17:58:02 +0530

    - cart page design

--- src/components/Cart/cartList.js
+++ src/components/Cart/cartList.js
@@ -1,16 +1,37 @@
 import React from 'react'
 import classes from './cartList.module.css'
 import { FaIndianRupeeSign } from 'react-icons/fa6'
+import { MdDelete } from 'react-icons/md'
+import { useNavigate } from 'react-router'
 
 const CartList = (item) => {
+    const navigate = useNavigate()
     return (
-        <div className={classes.cd}>
-            <div>{item.item.name}</div>
-            <div>
-                <FaIndianRupeeSign size={12} color={'#f0ece5'} />
-                {item.item.price}
+        <div className={classes.cartListWrapper}>
+            <div
+                className={classes.leftContain}
+                onClick={() => navigate(`/category/${item.item.categoryName}`)}
+            >
+                <img
+                    src={item.item.image}
+                    alt={item.sku}
+                    height={110}
+                    width={110}
+                />
             </div>
-            <div>{item.item.cartQuntity}</div>
+            <div className={classes.middleContain}>
+                <div className={classes.productName}>{item.item.name}</div>
+                <div className={classes.productPrice}>
+                    <FaIndianRupeeSign size={12} color="#161a30" />
+                    {item.item.price}
+                </div>
+                <div className={classes.cartQuntity}>
+                    {item.item.cartQuntity}
+                </div>
+            </div>
+            <div className={classes.rightcontain}>
+                <MdDelete size={24} color="#161a30" />
+            </div>
         </div>
     )
 }
